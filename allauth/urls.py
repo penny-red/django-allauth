a8255384de9e4a98283b12daebc939a5b34f6a39
Ben Whalley <benwhalley@gmail.com>
2017-04-18 15:51:43 +0100

    fix for django 1.11

--- allauth/urls.py
+++ allauth/urls.py
@@ -7,10 +7,10 @@ from allauth.socialaccount import providers
 from . import app_settings
 
 
-urlpatterns = [url(r'^', include('allauth.account.urls'))]
+urlpatterns = [url(r'^', include('allauth.account.urls')), ]
 
 if app_settings.SOCIALACCOUNT_ENABLED:
-    urlpatterns += [url(r'^social/', include('allauth.socialaccount.urls'))]
+    urlpatterns += [url(r'^social/', include('allauth.socialaccount.urls')), ]
 
 for provider in providers.registry.get_list():
     try:
